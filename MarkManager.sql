9ca9ec5446c71ebe698170750dfa98f8fb869715
Fmafu <107658869+Fmafu@users.noreply.github.com>
2022-07-14 17:03:47 +0700

    code

--- MarkManager.sql
+++ MarkManager.sql
@@ -1,106 +1,97 @@
-USE [Mark Manager]
-GO
 
-/****** Object:  Table [dbo].[Course]    Script Date: 6/19/2022 6:32:14 PM ******/
-SET ANSI_NULLS ON
-GO
 
-SET QUOTED_IDENTIFIER ON
-GO
+CREATE TABLE HomeTown(
+	HomeTownID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	HomeTownName NVARCHAR(30) NOT NULL
+)
 
-CREATE TABLE [dbo].[Course](
-	[CourseID] [varchar](50) NOT NULL,
-	[CourseName] [nchar](10) NOT NULL,
- CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED 
-(
-	[CourseID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+CREATE TABLE Student(
+	StudentID VARCHAR(25) NOT NULL PRIMARY KEY,
+	
+	StudentFirstName NVARCHAR(30) NOT NULL,
+	StudentLastName NVARCHAR(30) NOT NULL,
+	BirthDay DATE NOT NULL,
+	Sex BIT NOT NULL,
+	Major VARCHAR(55) NOT NULL,
+	ContacMail VARCHAR(70) NOT NULL,
+	HomeTownID INT NOT NULL FOREIGN KEY REFERENCES HomeTown(HomeTownID),	
+)
 
-USE [Mark Manager]
-GO
+CREATE TABLE Lecture(
+	LectureID VARCHAR(25) NOT NULL PRIMARY KEY,
+	LectureFirstName NVARCHAR(30) NOT NULL,
+	LectureLastName NVARCHAR(30) NOT NULL,
+	BirthDay DATE,
+	Sex BIT NOT NULL,
+	ContacMail VARCHAR(70) NOT NULL,
+	HomeTownID INT NOT NULL FOREIGN KEY REFERENCES HomeTown(HomeTownID),
+)
 
-/****** Object:  Table [dbo].[Group]    Script Date: 6/19/2022 6:33:11 PM ******/
-SET ANSI_NULLS ON
-GO
+CREATE TABLE DEPARTMENT(
+	DepartmentID INT NOT NULL PRIMARY KEY,
+	DepartmentName VARCHAR(70) NOT NULL,
+)
 
-SET QUOTED_IDENTIFIER ON
-GO
+CREATE TABLE Course(
+	CourseID VARCHAR(15) NOT NULL PRIMARY KEY,
+	CourseName VARCHAR(70) NOT NULL,
+	[Number of sessions] INT NOT NULL,
+	Department INT NOT NULL FOREIGN KEY REFERENCES DEPARTMENT(DepartmentID),
+	Semester VARCHAR(15) NOT NULL,
+)
 
-CREATE TABLE [dbo].[Group](
-	[GroupID] [varchar](50) NOT NULL,
-	[StudentID] [varchar](50) NOT NULL,
-	[CourseID] [varchar](50) NOT NULL,
-	[LecturerID] [int] NOT NULL,
- CONSTRAINT [PK_Group] PRIMARY KEY CLUSTERED 
-(
-	[GroupID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+CREATE TABLE [Group](
+	GroupID varchar(15) not null primary key,
+	CourseID VARCHAR(15) NOT NULL FOREIGN KEY REFERENCES Course(CourseID),
+	LectureID VARCHAR(25) NOT NULL FOREIGN KEY REFERENCES Lecture(LectureID),
 
-ALTER TABLE [dbo].[Group]  WITH CHECK ADD  CONSTRAINT [FK_Group_Course] FOREIGN KEY([CourseID])
-REFERENCES [dbo].[Course] ([CourseID])
-GO
+)
 
-ALTER TABLE [dbo].[Group] CHECK CONSTRAINT [FK_Group_Course]
-GO
+CREATE TABLE StudentGroup(
+	GroupID VARCHAR(15) NOT NULL FOREIGN KEY REFERENCES [Group](GroupID),
+	StudentID VARCHAR(25) NOT NULL FOREIGN KEY REFERENCES Student(StudentID),
+	
+	CONSTRAINT pk_StudentID_CourseID_GroupID PRIMARY KEY (GroupID,StudentID),
+	EnrolledDate DATE NOT NULL,
+)
 
-ALTER TABLE [dbo].[Group]  WITH CHECK ADD  CONSTRAINT [FK_Group_Lecturer] FOREIGN KEY([LecturerID])
-REFERENCES [dbo].[Lecturer] ([LecturerID])
-GO
+CREATE TABLE Class(
+	ClassID VARCHAR(80) NOT NULL PRIMARY KEY,
+	LectureID VARCHAR(25) NOT NULL FOREIGN KEY REFERENCES Lecture(LectureID),
+)
 
-ALTER TABLE [dbo].[Group] CHECK CONSTRAINT [FK_Group_Lecturer]
-GO
+CREATE TABLE Element(
+	Category VARCHAR(40) NOT NULL PRIMARY KEY,
+	CourseID VARCHAR(15) NOT NULL FOREIGN KEY REFERENCES Course(CourseID),
+	StartDate DATE NOT NULL,
+	EndDate DATE NOT NULL,
+)
 
-ALTER TABLE [dbo].[Group]  WITH CHECK ADD  CONSTRAINT [FK_Group_Student] FOREIGN KEY([StudentID])
-REFERENCES [dbo].[Student] ([StudentID])
-GO
+CREATE TABLE Assessment(
+	AssessmentID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Category VARCHAR(40) NOT NULL FOREIGN KEY REFERENCES Element(Category),
+	[Type] NVARCHAR(15) NOT NULL,
+	Part INT NOT NULL,
+	[Weight] Float check([Weight]<1 and [Weight]>0) NOT NULL,
+	[Completion Criteria] INT NOT NULL,
+	[No Question] INT,
+	
+)
 
-ALTER TABLE [dbo].[Group] CHECK CONSTRAINT [FK_Group_Student]
-GO
+CREATE TABLE Process(
+	StudentID VARCHAR(25) NOT NULL FOREIGN KEY REFERENCES Student(StudentID),
+	Category VARCHAR(40) NOT NULL FOREIGN KEY REFERENCES Element(Category),
+	CONSTRAINT pk_StudentID_Category PRIMARY KEY (StudentID,Category),
+	SubmisstionStatus BIT NOT NULL,
+)
 
-USE [Mark Manager]
-GO
 
-/****** Object:  Table [dbo].[Lecturer]    Script Date: 6/19/2022 6:33:34 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Lecturer](
-	[LecturerID] [int] NOT NULL,
-	[LecturerName] [varchar](50) NOT NULL,
-	[Email] [varchar](50) NOT NULL,
- CONSTRAINT [PK_Lecturer] PRIMARY KEY CLUSTERED 
-(
-	[LecturerID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-USE [Mark Manager]
-GO
-
-/****** Object:  Table [dbo].[Student]    Script Date: 6/19/2022 6:33:47 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Student](
-	[StudentID] [varchar](50) NOT NULL,
-	[StudentName] [nvarchar](50) NOT NULL,
-	[DoB] [date] NOT NULL,
-	[Gender] [bit] NOT NULL,
-	[Address] [varchar](50) NOT NULL,
- CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED 
-(
-	[StudentID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+CREATE TABLE Result(
+	ResultID INT NOT NULL,
+	AssessmentID INT NOT NULL FOREIGN KEY REFERENCES Assessment(AssessmentID),
+	CONSTRAINT pk_OutputID_Graded_by_AssessmentID PRIMARY KEY (ResultID,AssessmentID),
+	[Day of publication] DATE NOT NULL,
+	Mark FLOAT,
+	[Status] BIT,
+)
 
